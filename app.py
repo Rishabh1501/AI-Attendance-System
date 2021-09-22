fd53966144164f36384fd3c114553cdf1f74f07d
Rishabh Kalra <rishabhkalra1501@gmail.com>
2021-09-22 17:09:28 +0530

    mongoDb added

--- app.py
+++ app.py
@@ -5,27 +5,38 @@ import json
 # custom package imports
 from data_ingestion.data_import_and_preprocessing import DataImport, Preprocessing
 from attendance_api_functions.api_functions import API_Functions
+from database_api_functions.db_api_functions import DatabaseAPI
 
 # instatiate flask app
 app = Flask(__name__, template_folder='./templates')
+
 
 # making objects
 camera = cv2.VideoCapture(0)  # change this if camera not working
 data_import = DataImport()
 preprocessing = Preprocessing()
 
+
 # loading config.json
 with open('config.json') as f:
     config_file = json.load(f)
 
+#creating database object
+database = DatabaseAPI(camera, "mongodb://localhost:27017/",
+                       "Attendance", config_file["saved_image_folder"])
+
+
 # making all the folders
 data_import.make_folders(
-    config_file["saved_image_folder"], config_file["attendance_folder_path"],config_file["image_path"])
+    config_file["saved_image_folder"], config_file["attendance_folder_path"], config_file["image_path"])
 
-#creating csv file
-attendance_file_path = data_import.make_csv_file()
+# creating csv file
+# attendance_file_path = data_import.make_csv_file()
 
-#calling important functions
+#creating collection
+database.make_database_collection()
+
+# calling important functions
 images, known_face_names = data_import.read_images(
     path=config_file["image_path"])
 known_face_encodings = preprocessing.faceEncodings(images)
@@ -34,11 +45,11 @@ api_functions = API_Functions(
 
 
 
-
 @app.route('/')
 def index():
-    global attendance_file_path
-    attendance_file_path = data_import.make_csv_file()
+    # global attendance_file_path
+    # attendance_file_path = data_import.make_csv_file()
+    database.make_database_collection()
     return render_template('index.html')
 
 
@@ -50,16 +61,18 @@ def video_feed():
 @app.route('/checkin', methods=['POST'])
 def checkin():
     name = request.form.get("name")
-    status = api_functions.check_in(
-        name, attendance_file_path, save_image=True)
+    # status = api_functions.check_in(
+    #     name, attendance_file_path, save_image=True)
+    status = database.check_in(name,save_image=True)
     return render_template('result.html', status='Checked In Status For {} : {} '.format(name, status))
 
 
 @app.route('/checkout', methods=['POST'])
 def checkout():
     name = request.form.get("name")
-    status = api_functions.check_out(
-        name, attendance_file_path, save_image=True)
+    # status = api_functions.check_out(
+    #     name, attendance_file_path, save_image=True)
+    status = database.check_out(name,save_image=True)
     return render_template('result.html', status='Checked Out Status {} : {} '.format(name, status))
 
 
